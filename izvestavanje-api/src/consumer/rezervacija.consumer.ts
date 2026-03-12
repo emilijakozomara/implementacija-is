3f1687b0a4bb2da2f3d39cc77788d17fc6d286c6
Emilija Kozomara <emakozomara16@email.com>
2026-03-12 17:34:27 +0100

    Izmjena na portalu

--- izvestavanje-api/src/consumer/rezervacija.consumer.ts
+++ izvestavanje-api/src/consumer/rezervacija.consumer.ts
@@ -8,19 +8,16 @@ export class RezervacijaConsumer {
 
   @MessagePattern('KREIRANA')
   async handleKreirana(@Payload() payload: any) {
-    console.log('KREIRANA RAW PAYLOAD:', JSON.stringify(payload));
     await this.izvestajService.handleKreirana(payload);
   }
 
   @MessagePattern('IZMENJENA')
   async handleIzmenjena(@Payload() payload: any) {
-    console.log('IZMENJENA RAW PAYLOAD:', JSON.stringify(payload));
     await this.izvestajService.handleIzmenjena(payload);
   }
 
   @MessagePattern('OTKAZANA')
   async handleOtkazana(@Payload() payload: any) {
-    console.log('OTKAZANA RAW PAYLOAD:', JSON.stringify(payload));
     await this.izvestajService.handleOtkazana(payload);
   }
 }
